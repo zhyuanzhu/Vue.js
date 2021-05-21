7c00da2b701b63f27c95cb510fd61954222a1f79
alphazhangyz <alphazhangyz@gmail.com>
2021-05-21 22:30:33 +0800

    分析 global-api --注册插件

--- src/core/global-api/index.js
+++ src/core/global-api/index.js
@@ -29,11 +29,14 @@ export function initGlobalAPI (Vue: GlobalAPI) {
       )
     }
   }
+  // 给 Vue 添加 config 属性的监听，如果设置了 set ，在开发环境报出异常提醒
   Object.defineProperty(Vue, 'config', configDef)
 
   // exposed util methods.
   // NOTE: these are not considered part of the public API - avoid relying on
   // them unless you are aware of the risk.
+
+  // 不对外公开的方法 util
   Vue.util = {
     warn,
     extend,
@@ -41,28 +44,48 @@ export function initGlobalAPI (Vue: GlobalAPI) {
     defineReactive
   }
 
+  // 给 Vue 添加静态 set 方法
   Vue.set = set
+  // 给 Vue 添加静态 delete 方法
   Vue.delete = del
+  // 给 Vue 添加静态 nextTick 方法
   Vue.nextTick = nextTick
 
   // 2.6 explicit observable API
+  // 给 Vue 添加静态方法 observable 将 obj 对象转成 响应式对象
   Vue.observable = <T>(obj: T): T => {
     observe(obj)
     return obj
   }
 
+  // 给 Vue 添加静态属性 options = Object.create(null)
   Vue.options = Object.create(null)
+  // 给 Vue 的静态属性 options 挂载 components, directives, filters
+  // ['component', 'directive', 'filter']
   ASSET_TYPES.forEach(type => {
     Vue.options[type + 's'] = Object.create(null)
   })
+  /**
+   * Vue.options = {
+   *     components: {},
+   *     directives: {},
+   *     filters: {}
+   * }
+   */
 
   // this is used to identify the "base" constructor to extend all plain-object
   // components with in Weex's multi-instance scenarios.
+  // 给 Vue 的静态属性 options 挂载 _base = Vue
   Vue.options._base = Vue
 
+  // extend(to, from) 将 from 浅拷贝至 to； 如果 key 重复，from 对应的属性或方法覆盖 to 原有的属性或方法
+  // builtInComponents 后续查看
   extend(Vue.options.components, builtInComponents)
 
+  // 给 Vue 注册插件
   initUse(Vue)
+
+  // 
   initMixin(Vue)
   initExtend(Vue)
   initAssetRegisters(Vue)
--- src/core/global-api/use.js
+++ src/core/global-api/use.js
@@ -4,19 +4,27 @@ import { toArray } from '../util/index'
 
 export function initUse (Vue: GlobalAPI) {
   Vue.use = function (plugin: Function | Object) {
+    // 如果 Vue 中 有 _installedPlugins 返回否则返回 []
     const installedPlugins = (this._installedPlugins || (this._installedPlugins = []))
+    // 存在这个plugin 则直接返回
     if (installedPlugins.indexOf(plugin) > -1) {
       return this
     }
 
     // additional parameters
+    // 将 arguments 从第一项开始，将后面所有项存放至一个数组中，顺序保持不变
     const args = toArray(arguments, 1)
+    // 将 Vue 添加至数组的第一项
     args.unshift(this)
+    // 如果传入的 plugin 的 install 属性是一个 function
     if (typeof plugin.install === 'function') {
+      // 调用 plugin 的 install 方法，并且传入 args 数组，且该方法的 this 指向 plugin
       plugin.install.apply(plugin, args)
     } else if (typeof plugin === 'function') {
+      // 如果 plugin 本身是一个 function, 直接调用，将参数传入
       plugin.apply(null, args)
     }
+    // 将 plugin push 进数组 installedPlugins 中
     installedPlugins.push(plugin)
     return this
   }
