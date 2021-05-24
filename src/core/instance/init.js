f14be88d9024aebb555a4367bf37f8171ac6c2ae
alphazhangyz <alphazhangyz@gmail.com>
2021-05-24 11:55:56 +0800

    给 vue 挂载off on off off off off off off off on off on off off off off off on off off off on on off off off off on off off off off off off off off off off off off on off off on off off off on on on on off off off on off off on off off on off off on off on off off on off on off off off off on off off off on off off on off off off off off off off off on off on off off on off off off off off off off on off off off off on off on off on off on off on on off on off off on on on off on on off on on on on off off off off on on off off on off off off off off on off off on off off on on off off off off off on off off off off on on off on off off on off off on off on off off off off off off off off on on off on off off off属性

--- src/core/instance/init.js
+++ src/core/instance/init.js
@@ -40,6 +40,7 @@ export function initMixin (Vue: Class<Component>) {
       // optimize internal component instantiation
       // since dynamic options merging is pretty slow, and none of the
       // internal component options needs special treatment.
+      
       initInternalComponent(vm, options)
     } else {
       vm.$options = mergeOptions(
